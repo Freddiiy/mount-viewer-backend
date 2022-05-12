a62093f22b1c7932e2a1dcfa3e97cfcdf9a37617
Freddiiy <frederikgaller@live.dk>
2022-05-12 15:12:43 +0200

    added realm on character

--- src/main/java/dtos/RealmDTO.java
+++ src/main/java/dtos/RealmDTO.java
@@ -1,17 +1,9 @@
 package dtos;
 
 public class RealmDTO {
-    private KeyDTO key;
     private String name;
     private int id;
     private String slug;
-
-    public RealmDTO(KeyDTO key, String name, int id, String slug) {
-        this.key = key;
-        this.name = name;
-        this.id = id;
-        this.slug = slug;
-    }
 
     public RealmDTO(String name, int id, String slug) {
         this.name = name;
@@ -19,13 +11,6 @@ public class RealmDTO {
         this.slug = slug;
     }
 
-    public KeyDTO getKey() {
-        return key;
-    }
-
-    public void setKey(KeyDTO key) {
-        this.key = key;
-    }
 
     public String getName() {
         return name;
